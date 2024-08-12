4020cd98365bda53802d7f6e507531b88f3f5221
chunche95 <37621958+chunche95@users.noreply.github.com>
2024-08-12 17:11:02 +0200

    Add: PacketTracer, CCNA1, CCNA2, Softskill

---
+++ Redes/CCNA-1/MyTower.sh
@@ -0,0 +1,39 @@
+
+#!/bin/sh
+
+# Modificacion del script
+git add subida.sh &&
+sleep 2
+# Principal
+git add Exploraciondelared.html &&
+git nota "Introduccion a las redes - Networking Academy by Cisco" &&
+sleep 2
+echo "Archivo(s) de exploracion subido"
+# Imagenes
+git add images/. &&
+git nota "Media - img" &&
+echo "Archivo(s) de imagen(es) OK!"
+sleep 1
+# Videos
+git add videos/. &&
+git nota "Media - videos" &&
+echo "Archivo(s) de video(s) OK!"
+# Documentos
+git add documentos/. &&
+git nota "Media - Docs" &&
+echo "Archivo(s) docs OK!"
+git add Archivos/. &&
+git nota "Media - Others" &&
+sleep 2
+# Other - Otros.
+git add . &&
+git nota "Others" && 
+sleep 1
+clear
+git push origin master
+echo "Otros archivos  subidos"
+sleep 2
+echo "+-------------------------------------+"
+echo "| Todos los trabajos han finalizado   |"
+echo "+-------------------------------------+"
+# clear

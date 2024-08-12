c973fe32351893b47c9c2ac9e2ab83dbb7d54c59
chunche95 <37621958+chunche95@users.noreply.github.com>
2024-08-12 17:04:34 +0200

    Add: Seguridad Informatica

---
+++ CyberOpsv1.0/cyberOps/up.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+git add css/*
+git nota "Style"
+git sube
+git add fonts/*
+git nota "Fonts"
+git sube
+
+git add images/*
+git nota "Images"
+git sube
+
+git add js/*
+git nota "JavaScript"
+git sube
+
+git add php/*
+git nota "PHP-Contact"
+git sube
+
+git add videos/*
+git nota "Videos"
+git sube
+
+git add index.html
+git nota "Main page"
+git sube
+
+git add capítulos/*
+git nota "Chapters"
+git sube
+
+git add software/*
+git nota "SW"
+git sube
+
+git add docs/1/*
+git nota "Docs1"
+git add docs/2/*
+git nota "Docs2"
+git add docs/3/*
+git nota "Docs3"
+git add docs/4/*
+git nota "Docs4"
+git add docs/5/*
+git nota "Docs6"
+git add docs/7/*
+git nota "Docs7"
+git add docs/8/*
+git nota "Docs8"
+git add docs/9/*
+git nota "Docs9"
+git sube
+
+
+git add README.md
+git nota "README"
+git sube
+
+git add up.sh
+git nota "GitHub"
+git sube
+
+sleep 5
+clear
+git status
